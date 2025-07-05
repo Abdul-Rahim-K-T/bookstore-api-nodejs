c52119af1e4cd68959692a55bc76e08526ded441
Abdul Rahim K T <abdulrahimkt653@gmail.com>
2025-07-05 08:57:13 +0530

    restructuring in mvc with clear

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(json());
 app.use('/api/users', userRoutes);
 
+app.use((req, res) =>{
+    res.status(404).json({ message: 'Route not found' });
+});
+
 
 app.listen(process.env.PORT, ()=> {
     console.log(`Server is running on port ${process.env.PORT}`);
